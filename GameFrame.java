710918499c55678fb0f106c790917303bbb4e46d
JakubSzym <kubaszym77@gmail.com>
2022-06-03 19:40:46 +0200

    Added frog and obstacles

--- GameFrame.java
+++ GameFrame.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
+
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class GameFrame extends JFrame implements ActionListener {
   GamePanel game;
   JButton resetButton;
+  JLabel label;
+  static final String DATAFILE = "record.dat";
 
   GameFrame() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,7 +20,7 @@ public class GameFrame extends JFrame implements ActionListener {
     resetButton.setSize(100, 50);
     resetButton.setLocation(0, 0);
     resetButton.addActionListener(this);
-
+    readHighestScore();
     game = new GamePanel();
 
     this.add(resetButton);
@@ -23,12 +29,30 @@ public class GameFrame extends JFrame implements ActionListener {
     this.setVisible(true);
   }
 
+  public void readHighestScore()  {
+    Scanner istream = null;
+    File datafile = new File(DATAFILE);
+    try {
+      istream = new Scanner(datafile);
+    } catch (FileNotFoundException e) {
+      System.out.println("Cannot open file");
+    }
+
+    int record = istream.nextInt();
+    label = new JLabel("Record: " + record);
+    label.setBounds(0, 100,
+                    label.getPreferredSize().width, 
+                    label.getPreferredSize().height);
+    this.add(label);
+  }
   @Override
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() == resetButton) {
       this.remove(game);
+      this.remove(label);
       game = new GamePanel();
       this.add(game);
+      readHighestScore();
       SwingUtilities.updateComponentTreeUI(this);
     }
   }
--- GamePanel.java
+++ GamePanel.java
@@ -1,16 +1,23 @@
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.Random;
+
 
 public class GamePanel extends JPanel implements ActionListener {
 
   static final int WIDTH = 500;
   static final int HEIGHT = 500;
   static final int UNIT_SIZE = 10;
+  static final int ELEMENTS = 5;
+  static final int OBSTACLES_NUM = 8;
   static final int GAME_UNITS = (WIDTH * HEIGHT) / UNIT_SIZE;
-  static final int DELAY = 50;
+  static final int DELAY = 20;
+  static final String DATAFILE = "record.dat";
 
   final int xCoord[] = new int[GAME_UNITS];
   final int yCoord[] = new int[GAME_UNITS];
@@ -20,13 +27,23 @@ public class GamePanel extends JPanel implements ActionListener {
   int segments = 6;
   int aiSegments = 6;
   int eatenFruits;
+  int aiEatenFruits;
+  int record;
   int xFruit;
   int yFruit;
+  int xFrog;
+  int yFrog;
+  int[][] obstaclesX;
+  int[][] obstaclesY;
   boolean isRunning = false;
+  boolean gamerWon = false;
+  boolean aiWon = false;
   Timer timer;
   Random random;
   Direction direction = Direction.RIGHT;
   Direction aiDirection = Direction.LEFT;
+  Direction frogDirection = Direction.LEFT;
+  File datafile;
 
   Action turnUpAction = new AbstractAction() {
     public void actionPerformed(ActionEvent e) {
@@ -63,6 +80,17 @@ public class GamePanel extends JPanel implements ActionListener {
   GamePanel() {
     random = new Random();
 
+    Scanner istream = null;
+    datafile = new File(DATAFILE);
+    
+    try {
+      istream = new Scanner(datafile);
+    } catch (FileNotFoundException e) {
+      System.out.println("Cannot open file");
+    }
+
+    record = istream.nextInt();
+
     this.setSize(WIDTH, HEIGHT);
     this.setLocation(100, 0);
     this.setBackground(Color.DARK_GRAY);
@@ -78,14 +106,19 @@ public class GamePanel extends JPanel implements ActionListener {
     this.getActionMap().put("turnDown", turnDownAction);
     this.getActionMap().put("turnLeft", turnLeftAction);
     this.getActionMap().put("turnRight", turnRightAction);
+  
+    for (int i = 0; i < aiSegments; i++) {
+      aiXCoord[i] = WIDTH;
+      aiYCoord[i] = HEIGHT;
+    }
 
     start();
   }
 
   public void start() {
+    putFrog();
+    putObstacles();
     putFruit();
-    aiXCoord[0] = HEIGHT;
-    aiYCoord[0] = WIDTH;
     isRunning = true;
     timer = new Timer(DELAY, this);
     timer.start();
@@ -143,12 +176,30 @@ public class GamePanel extends JPanel implements ActionListener {
         }
       }
 
+      //
+      // frog
+      //
+      g.setColor(Color.blue);
+      g.fillOval(xFrog, yFrog, UNIT_SIZE, UNIT_SIZE);
+
+      //
+      // obstacles
+      //
+
+      for (int i = 0; i < OBSTACLES_NUM; i++)
+      {
+        for (int j = 0; j < ELEMENTS; j++)
+        {
+          g.setColor(Color.black);
+          g.fillRect(obstaclesX[i][j], obstaclesY[i][j], UNIT_SIZE, UNIT_SIZE);
+        }
+      }
 
       g.setColor(Color.red);
       g.setFont(new Font("Arial", Font.BOLD, 10));
       FontMetrics metrics = getFontMetrics(g.getFont());
-      g.drawString("Score: " + eatenFruits,
-          (WIDTH - metrics.stringWidth("Score: " + eatenFruits)) / 2,
+      g.drawString("You: " + eatenFruits + " AI: " + aiEatenFruits,
+          (WIDTH - metrics.stringWidth("You: " + eatenFruits + " AI: " + aiEatenFruits)) / 2,
           g.getFont().getSize());
     } else {
       gameOver(g);
@@ -173,6 +224,166 @@ public class GamePanel extends JPanel implements ActionListener {
         break;
       case LEFT:
         xCoord[0] -= UNIT_SIZE;
+        break;
+    }
+  }
+
+  public void moveFrog() {
+    if (xFrog == WIDTH - UNIT_SIZE && yFrog == 0) {
+      switch (frogDirection) {
+        case RIGHT:
+          frogDirection = Direction.DOWN;
+          break;
+        case UP:
+          frogDirection = Direction.LEFT;
+          break;
+        default:
+          break;
+      }
+    }
+
+    if (xFrog == 0 && yFrog == HEIGHT - UNIT_SIZE) {
+      switch (frogDirection) {
+        case LEFT:
+          frogDirection = Direction.UP;
+          break;
+        case DOWN:
+          frogDirection = Direction.RIGHT;
+          break;
+        default:
+          break;
+      }
+    }
+
+    switch (xFrog) {
+      case 0:
+        if (frogDirection == Direction.LEFT) {
+          frogDirection = Direction.DOWN;
+        }
+        break;
+      case WIDTH - UNIT_SIZE:
+        if (frogDirection == Direction.RIGHT) {
+          frogDirection = Direction.UP;
+        }
+        break;
+    }
+
+    switch (yFrog) {
+      case 0:
+        if (frogDirection == Direction.UP) {
+          frogDirection = Direction.LEFT;
+        }
+        break;
+      case HEIGHT - UNIT_SIZE:
+        if (frogDirection == Direction.DOWN) {
+          frogDirection = Direction.RIGHT;
+        }
+        break;
+    }
+
+    if (xFrog == 0 && yFrog == 0) {
+      switch (frogDirection) {
+        case UP:
+          frogDirection = Direction.RIGHT;
+          break;
+        case LEFT:
+          frogDirection = Direction.DOWN;
+          break;
+        default:
+          break;
+      }
+    }
+    if (xFrog == WIDTH - UNIT_SIZE && yFrog == HEIGHT - UNIT_SIZE) {
+      switch (frogDirection) {
+        case DOWN:
+          frogDirection = Direction.LEFT;
+          break;
+        case RIGHT:
+          frogDirection = Direction.UP;
+          break;
+        default:
+          break;
+      }
+    }
+
+    for (int i  = 1; i < segments; i++) {
+      if (xCoord[i] == xFrog && yCoord[i] == yFrog) {
+        switch (frogDirection)
+        {
+          case RIGHT:
+            frogDirection = Direction.LEFT;
+            break;
+          case DOWN:
+            frogDirection = Direction.UP;
+            break;
+          case UP:
+            frogDirection = Direction.DOWN;
+            break;
+          case LEFT:
+            frogDirection = Direction.RIGHT;
+            break;
+        }
+      }
+    }
+
+    for (int i  = 1; i < aiSegments; i++) {
+      if (aiXCoord[i] == xFrog && aiYCoord[i] == yFrog) {
+        switch (frogDirection)
+        {
+          case RIGHT:
+            frogDirection = Direction.LEFT;
+            break;
+          case DOWN:
+            frogDirection = Direction.UP;
+            break;
+          case UP:
+            frogDirection = Direction.DOWN;
+            break;
+          case LEFT:
+            frogDirection = Direction.RIGHT;
+            break;
+        }
+      }
+    }
+
+    for (int i = 0; i < OBSTACLES_NUM; i++)
+    {
+      for (int j = 0; j < ELEMENTS; j++)
+      {
+        if (Math.abs(xFrog - obstaclesX[i][j])  == UNIT_SIZE && 
+            Math.abs(yFrog - obstaclesY[i][j]) == 0) {
+            if (xFrog < obstaclesX[i][j] && frogDirection == Direction.RIGHT) {
+              frogDirection = Direction.UP;
+            }
+            if (xFrog > obstaclesX[i][j] && frogDirection == Direction.LEFT) {
+              frogDirection = Direction.DOWN;
+            }
+        }
+
+        if (Math.abs(xFrog - obstaclesX[i][j])  == 0 && 
+            Math.abs(yFrog - obstaclesY[i][j]) == UNIT_SIZE) {
+          if (yFrog < obstaclesY[i][j] && frogDirection == Direction.DOWN) {
+            frogDirection = Direction.LEFT;
+          }
+          if (yFrog > obstaclesY[i][j] && frogDirection == Direction.UP) {
+            frogDirection = Direction.RIGHT;
+          }
+        }
+      }
+    }
+
+    switch (frogDirection) {
+      case UP:
+        yFrog -= UNIT_SIZE;
+        break;
+      case DOWN:
+        yFrog += UNIT_SIZE;
+        break;
+      case RIGHT:
+        xFrog += UNIT_SIZE;
+        break;
+      case LEFT:
+        xFrog -= UNIT_SIZE;
         break;
     }
   }
@@ -291,6 +502,32 @@ public class GamePanel extends JPanel implements ActionListener {
       }
     }
 
+    for (int i = 0; i < OBSTACLES_NUM; i++)
+    {
+      for (int j = 0; j < ELEMENTS; j++)
+      {
+        if (Math.abs(aiXCoord[0] - obstaclesX[i][j])  == UNIT_SIZE && 
+            Math.abs(aiYCoord[0] - obstaclesY[i][j]) == 0) {
+            if (aiXCoord[0] < obstaclesX[i][j] && aiDirection == Direction.RIGHT) {
+              aiDirection = Direction.UP;
+            }
+            if (aiXCoord[0] > obstaclesX[i][j] && aiDirection == Direction.LEFT) {
+              aiDirection = Direction.DOWN;
+            }
+        }
+
+        if (Math.abs(aiXCoord[0] - obstaclesX[i][j])  == 0 && 
+            Math.abs(aiYCoord[0] - obstaclesY[i][j]) == UNIT_SIZE) {
+          if (aiYCoord[0] < obstaclesY[i][j] && aiDirection == Direction.DOWN) {
+            aiDirection = Direction.LEFT;
+          }
+          if (aiYCoord[0] > obstaclesY[i][j] && aiDirection == Direction.UP) {
+          aiDirection = Direction.RIGHT;
+          }
+        }
+      }
+    }
+
     for (int i = aiSegments; i > 0; i--) {
       aiXCoord[i] = aiXCoord[i - 1];
       aiYCoord[i] = aiYCoord[i - 1];
@@ -312,15 +549,83 @@ public class GamePanel extends JPanel implements ActionListener {
     }
   }
 
+  public void putObstacles() {
+    obstaclesX = new int[OBSTACLES_NUM][ELEMENTS];
+    obstaclesY = new int[OBSTACLES_NUM][ELEMENTS];
+    for (int i = 0; i < OBSTACLES_NUM; i++)
+    {
+      obstaclesX[i][0] = random.nextInt((WIDTH - 5*UNIT_SIZE) / UNIT_SIZE) * UNIT_SIZE;
+      obstaclesY[i][0] = random.nextInt((HEIGHT - 5*UNIT_SIZE) / UNIT_SIZE) * UNIT_SIZE;
+      for (int j = 1; j < ELEMENTS; j++) {
+        if (i % 2 == 0) {
+          obstaclesX[i][j] = obstaclesX[i][0];
+          obstaclesY[i][j] = obstaclesY[i][0] + j*UNIT_SIZE;
+        } else {
+          obstaclesX[i][j] = obstaclesX[i][0] + j*UNIT_SIZE;
+          obstaclesY[i][j] = obstaclesY[i][0];
+        }
+      }
+    }
+  }
+
   public void putFruit() {
     xFruit = random.nextInt((int) (WIDTH / UNIT_SIZE)) * UNIT_SIZE;
     yFruit = random.nextInt((int) (HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+
+    for (int i = 0; i < OBSTACLES_NUM; i++)
+    {
+      for (int j = 0; j < ELEMENTS; j++) {
+        if (xFruit == obstaclesX[i][j] && yFruit == obstaclesY[i][j]) {
+          putFruit();
+        }
+      }
+    }    
+  }
+
+  public void putFrog() {
+    xFrog = WIDTH / 2;
+    yFrog = HEIGHT / 2;
+    int dir = random.nextInt(4);
+    switch(dir) {
+      case 0:
+        frogDirection = Direction.RIGHT;
+        break;
+      case 1:
+        frogDirection = Direction.LEFT;
+        break;
+      case 2:
+        frogDirection = Direction.UP;
+        break;
+      case 3:
+        frogDirection = Direction.DOWN;
+        break;
+    }
   }
 
   public void checkFruit() {
     if (xCoord[0] == xFruit && yCoord[0] == yFruit) {
       segments++;
       eatenFruits++;
+      int newFrogDirection = random.nextInt(2);
+      switch (newFrogDirection) {
+        case 0:
+          if (frogDirection == Direction.UP) {
+            frogDirection = Direction.RIGHT;
+          }
+          if (frogDirection == Direction.RIGHT) {
+            frogDirection = Direction.DOWN;
+          }
+          break;
+        case 1:
+          if (frogDirection == Direction.DOWN) {
+            frogDirection = Direction.LEFT;
+          }
+          if (frogDirection == Direction.LEFT) {
+            frogDirection = Direction.UP;
+          }
+          break;
+      }
+
       putFruit();
     }
   }
@@ -328,7 +633,24 @@ public class GamePanel extends JPanel implements ActionListener {
   public void aiCheckFruit() {
     if (aiXCoord[0] == xFruit && aiYCoord[0] == yFruit) {
       aiSegments++;
+      aiEatenFruits++;
       putFruit();
+    }
+  }
+
+  public void checkFrog() {
+    if (xCoord[0] == xFrog && yCoord[0] == yFrog) {
+      segments++;
+      eatenFruits += 3;
+      putFrog();
+    }
+  }
+
+  public void aiCheckFrog() {
+    if (aiXCoord[0] == xFrog && aiYCoord[0] == yFrog) {
+      aiSegments++;
+      aiEatenFruits += 3;
+      putFrog();
     }
   }
 
@@ -343,36 +665,124 @@ public class GamePanel extends JPanel implements ActionListener {
       isRunning = false;
     }
 
+    for (int i = 0; i < OBSTACLES_NUM; i++)
+    {
+      for (int j = 0; j < ELEMENTS; j++)
+      {
+        if (xCoord[0] == obstaclesX[i][j] && yCoord[0] == obstaclesY[i][j]) {
+          isRunning = false;
+        }
+      }
+    }
+
+    for (int i = segments; i > 0; i--) {
+      for (int j = aiSegments; j > 0; j--) {
+        if (xCoord[i] == aiXCoord[j] && yCoord[i] == aiYCoord[j]) {
+          isRunning = false;
+        }
+      }
+    }
+
     if (!isRunning) {
+      aiEatenFruits += 5;
+      if (aiEatenFruits > eatenFruits) {
+        aiWon = true;
+      } else if (aiEatenFruits < eatenFruits) {
+        gamerWon = true;
+      }
+      timer.stop();
+    }
+  }
+
+  public void aiCheckCollisions() {
+    for (int i = aiSegments; i > 0; i--) {
+      if (aiXCoord[0] == aiXCoord[i] && aiYCoord[0] == aiYCoord[i]) {
+        isRunning = false;
+      }
+    }
+
+    if (aiXCoord[0] < 0 || aiXCoord[0] > WIDTH || aiYCoord[0] < 0 || aiYCoord[0] > HEIGHT) {
+      isRunning = false;
+      gamerWon = true;
+    }
+
+    for (int i = 0; i < OBSTACLES_NUM; i++)
+    {
+      for (int j = 0; j < ELEMENTS; j++)
+      {
+        if (aiXCoord[0] == obstaclesX[i][j] && aiYCoord[0] == obstaclesY[i][j]) {
+          isRunning = false;
+          gamerWon = true;
+        }
+      }
+    }
+
+    if (!isRunning) {
+      eatenFruits += 5;
+      if (eatenFruits > aiEatenFruits) {
+        gamerWon = true;
+      } else if (eatenFruits < aiEatenFruits) {
+        aiWon = true;
+      }
       timer.stop();
     }
   }
 
   public void gameOver(Graphics g) {
     this.setFocusable(false);
+
+    if (record < eatenFruits) {
+      PrintWriter ostream = null;
+      try {
+        ostream = new PrintWriter(datafile);
+      } catch (FileNotFoundException e) {
+        System.out.println("Cannot open file");
+      }
+
+      ostream.println(eatenFruits);
+      ostream.close();
+    }
     g.setColor(Color.red);
 
     g.setFont(new Font("Arial", Font.BOLD, 10));
     FontMetrics metrics1 = getFontMetrics(g.getFont());
-    g.drawString("Score: " + eatenFruits,
-        (WIDTH - metrics1.stringWidth("Score: " + eatenFruits)) / 2,
-        g.getFont().getSize());
+    g.drawString("You: " + eatenFruits + " AI: " + aiEatenFruits,
+          (WIDTH - metrics1.stringWidth("You: " + eatenFruits + " AI: " + aiEatenFruits)) / 2,
+          g.getFont().getSize());
 
     g.setFont(new Font("Arial", Font.BOLD, 20));
     FontMetrics metrics2 = getFontMetrics(g.getFont());
     g.drawString("Game Over",
         (WIDTH - metrics2.stringWidth("Game Over")) / 2,
         HEIGHT / 2);
+
+    if (gamerWon) {
+      g.drawString("You won!",
+                   (WIDTH - metrics2.stringWidth("You won!")) / 2,
+                   HEIGHT / 2 - 5*UNIT_SIZE);
+    } else if (aiWon) {
+      g.drawString("AI won!",
+                   (WIDTH - metrics2.stringWidth("AI won!")) / 2,
+                   HEIGHT / 2 - 5*UNIT_SIZE);
+    } else {
+      g.drawString("Draw",
+                   (WIDTH - metrics2.stringWidth("Draw")) / 2,
+                   HEIGHT / 2 - 5*UNIT_SIZE);
+    }
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
     if (isRunning) {
-      move();
+      // move();
+      moveFrog();
       aiMove();
-      checkFruit();
+      // checkFruit();
+      // checkFrog();
       aiCheckFruit();
-      checkCollisions();
+      aiCheckFrog();
+      // checkCollisions();
+      aiCheckCollisions();
     }
     repaint();
   }
